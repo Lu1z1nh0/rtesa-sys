f1fc569786c594385e7b61ef323ba908749e4ded
Lu1z1nh0 <plpm90_nath@hotmail.com>
2023-01-27 14:24:04 -0600

    ligeros cambios

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -50,10 +50,13 @@
                   <div class="mb-3">
                     <input id="password" class="form-control" type="password" placeholder="Contraseña" name="password" required/>
                     
-                    @error('message')        
-                      <p class="border border-red-500 rounded-md bg-red-100 w-fulltext-red-600 p-2 my-2">* {{ $message }}</p>
+                    @error('mensaje')        
+                      <div class="alert alert-danger border-2 d-flex align-items-center mt-3" role="alert">
+                        <div class="bg-danger me-3 icon-item"><span class="fas fa-times-circle text-white fs-3"></span></div>
+                        <p class="mb-0 flex-1">{{ $message }}</p><button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>
                     @enderror
-                    
+
                     <!--
                     @if($errors->any())
                     <div class="alert alert-danger" >
